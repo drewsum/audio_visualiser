544f9a4c1b3c51927d06391724be871e4e292962
Drew Maatman <dmaatman@xes-inc.com>
2026-03-26 18:14:51 -0500

    meh

--- scripts/messing_around.py
+++ scripts/messing_around.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal # type: ignore
 
-fc = [10, 100, 1000, 10000]  # Cutoff frequencies in Hz
+fc = [10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480]  # Cutoff frequencies in Hz
 
 # Plot the response
 plt.figure()
 
 for frequency in fc:
     # Design a Butterworth low-pass filter
-    b, a = signal.butter(4, [frequency*0.9, frequency*1.1], 'band', analog=True)
+    b, a = signal.butter(3, [frequency*0.9, frequency*1.1], 'band', analog=True)
     w, h = signal.freqs(b, a)
     plt.semilogx(w, 20 * np.log10(abs(h)))
 
